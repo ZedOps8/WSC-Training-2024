18550a8cc4b891d1c5c5f22011ff721653801ecb
Zed <gyana.cse7@gmail.com>
2023-09-01 13:11:32 +0530

    day13-commit

---
+++ day13-projectEc2ALB/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const productUtils = require('./utils/productutils');
+const axios = require('axios');
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/products', async (req, res) => {
+    try {
+        let response  = await productUtils.listProducts();
+        res.send(response);
+    } catch (error) {
+        console.log(`Error: ${error}`);
+        // 400 response code
+        res.sendStatus(400);
+    }
+});
+
+app.get('/ec2instance', async (req, res) => {
+    try {
+      // fetch instance id from http://169.254.169.254/latest/meta-data/instance-id
+      let response = await axios.get('http://169.254.169.254/latest/meta-data/instance-id');
+      res.send({instanceid: response.data});
+    } catch (error) {
+      console.log(`Error: ${error}`);
+      // 400 response code
+      res.sendStatus(400);
+    }
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
